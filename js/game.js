1908961dff56104445f16d4fcb8c92ff51260a59
Ian Nguyen <iannknguyen@gmail.com>
2015-11-01 00:40:44 -0700

    minor refactor again

--- js/game.js
+++ js/game.js
@@ -14,10 +14,6 @@
   Game.prototype.startGame = function() {
     gameOver = false;
     currentPiece = randomPiece();
-    $(document).on("keydown", function(e) {
-
-      app.game.playerInput(e);
-    });
 
     var raf;
 
@@ -97,56 +93,6 @@
     $("#points").text(app.game.score.toString());
     $("#lines").text(app.game.lines.toString());
     $("#speed").text(app.game.speed.toFixed(2) + "ms");
-  };
-
-  Game.prototype.playerInput = function(e) {
-    e.preventDefault();
-    switch(e.keyCode) {
-      case 32:
-      if (currentPiece.gridy < 0) {
-        return;
-      } else {
-      var rowLine = currentPiece.gridy;
-      while(app.game.board.validMove(currentPiece.gridx, rowLine + 1, currentPiece.currentState)) {
-        rowLine += 1;
-      }
-      currentPiece.gridy = rowLine;
-      dropped = true;
-      }
-      break;
-      case 37:
-      if(app.game.board.validMove(currentPiece.gridx-1, currentPiece.gridy, currentPiece.currentState)) {
-        currentPiece.gridx -= 1;
-      }
-        break;
-      case 38:
-      var newState;
-      if(currentPiece.currentState === currentPiece.states.length - 1) {
-        newState = 0;
-      } else {
-        newState = currentPiece.currentState+1;
-      }
-
-      if(currentPiece instanceof(LinePiece)) {
-      app.game.handleLineRotation(currentPiece, newState);
-      }
-      if(app.game.board.validMove(currentPiece.gridx, currentPiece.gridy, newState)) {
-        currentPiece.currentState = newState;
-      } else {
-        app.game.wallKick(currentPiece, newState);
-      }
-      break;
-      case 39:
-      if(app.game.board.validMove(currentPiece.gridx+1, currentPiece.gridy, currentPiece.currentState)) {
-        currentPiece.gridx += 1;
-      }
-        break;
-      case 40:
-      if(app.game.board.validMove(currentPiece.gridx, currentPiece.gridy+1, currentPiece.currentState)) {
-        currentPiece.gridy += 1;
-        break;
-      }
-    }
   };
 
   Game.prototype.drawBoard = function() {
--- js/view.js
+++ js/view.js
@@ -21,6 +21,60 @@ var app, canvas, ctx, blockImg, bgImg, currentPiece, imgLoader, previousTime, cu
     View.prototype.setup = function() {
       app = new Tetris.App();
       app.setup(canvas);
+      $(document).on("keydown", function(e) {
+        this.playerInput(e);
+      }.bind(this));
     };
+
+    View.prototype.playerInput = function(e) {
+      e.preventDefault();
+      switch(e.keyCode) {
+        case 32:
+        if (currentPiece.gridy < 0) {
+          return;
+        } else {
+        var rowLine = currentPiece.gridy;
+        while(app.game.board.validMove(currentPiece.gridx, rowLine + 1, currentPiece.currentState)) {
+          rowLine += 1;
+        }
+        currentPiece.gridy = rowLine;
+        dropped = true;
+        }
+        break;
+        case 37:
+        if(app.game.board.validMove(currentPiece.gridx-1, currentPiece.gridy, currentPiece.currentState)) {
+          currentPiece.gridx -= 1;
+        }
+          break;
+        case 38:
+        var newState;
+        if(currentPiece.currentState === currentPiece.states.length - 1) {
+          newState = 0;
+        } else {
+          newState = currentPiece.currentState+1;
+        }
+
+        if(currentPiece instanceof(LinePiece)) {
+        app.game.handleLineRotation(currentPiece, newState);
+        }
+        if(app.game.board.validMove(currentPiece.gridx, currentPiece.gridy, newState)) {
+          currentPiece.currentState = newState;
+        } else {
+          app.game.wallKick(currentPiece, newState);
+        }
+        break;
+        case 39:
+        if(app.game.board.validMove(currentPiece.gridx+1, currentPiece.gridy, currentPiece.currentState)) {
+          currentPiece.gridx += 1;
+        }
+          break;
+        case 40:
+        if(app.game.board.validMove(currentPiece.gridx, currentPiece.gridy+1, currentPiece.currentState)) {
+          currentPiece.gridy += 1;
+          break;
+        }
+      }
+    };
+
 
 })();
